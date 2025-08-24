03879d37041374cc5a809869d6e887bca8c767ef
‪Hazem Gmall‬‏ <161766768+hazem257@users.noreply.github.com>
2025-08-24 15:59:14 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ تفعيل CORS لجميع المواقع
-app.enableCors({
-  origin: [
-    'http://localhost:5173',       // وقت التطوير
-    'https://hazemgamal-sable.vercel.app/', // لينك الفرونت بعد الديبلاي
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-});
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',       
+      'https://your-vercel-app.vercel.app', 
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
