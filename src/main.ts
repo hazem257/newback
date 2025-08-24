d88719bdaae42b0af2bcb46827995e245f680d15
‪Hazem Gmall‬‏ <161766768+hazem257@users.noreply.github.com>
2025-08-24 21:13:44 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('PORT:', process.env.PORT);
+  console.log('Mongo URI:', process.env.MONGO_URI); // للتأكد من قيمة الـ URI
+
   const app = await NestFactory.create(AppModule);
 
-  // ✅ CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://hazemgamal-sable.vercel.app'], 
+    origin: [
+      'http://localhost:5173',
+      'https://hazemgamal-sable.vercel.app'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // ✅ خلي السيرفر يسمع على الـ PORT اللي Railway بيبعتُه
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`🚀 Server is running on port ${port}`);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  console.log(`🚀 Server is running on port ${process.env.PORT || 3000}`);
 }
 bootstrap();
