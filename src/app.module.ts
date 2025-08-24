33d09e7492131374508a481bd8c53ae2eba8e80d
‪Hazem Gmall‬‏ <161766768+hazem257@users.noreply.github.com>
2025-08-24 21:16:00 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,19 @@ import { ProjectsModule } from './projects/projects.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
 
+const mongoUrl = process.env.MONGO_URL;
+if (!mongoUrl) {
+  throw new Error('MONGO_URL environment variable is not defined');
+}
+
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI || ''),
+    ConfigModule.forRoot({
+      isGlobal: true, // بحيث كل الملفات تشوف المتغيرات
+    }),
+    MongooseModule.forRoot(mongoUrl),
     MulterModule.register({
-      storage: memoryStorage(), // لتخزين الملفات مؤقتًا بالذاكرة
+      storage: memoryStorage(), // تخزين مؤقت للملفات
     }),
     ProjectsModule,
   ],
