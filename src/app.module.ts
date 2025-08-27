ba05e092ec965f1bef99d0e0122da29ffa0101d6
‪Hazem Gamal <161766768+hazem257@users.noreply.github.com>
2025-08-27 15:29:23 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,7 @@ import { ProjectsModule } from './projects/projects.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
 import { CertificatesModule } from './certificates/certificates.module';
-
-const mongoUrl = 'mongodb+srv://dragon:dragon211004@coursesdb.mo9saja.mongodb.net/portfolio';
+const mongoUrl = process.env.MONGO_URL;
 if (!mongoUrl) {
   throw new Error('MONGO_URL environment variable is not defined');
 }
